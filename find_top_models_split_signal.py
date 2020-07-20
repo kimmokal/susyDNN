8e49911ce15c7631058c46612d33d0769257cb6f
Kallonen Kimmo T S <kimmokal@dx6-flafo-02.theorphys.helsinki.fi>
2020-07-20 18:08:13 +0300

    Separate model finders for one and split signals

--- find_top_models_split_signal.py
+++ find_top_models_split_signal.py
@@ -4,6 +4,16 @@ import sys
 import argparse
 import pandas as pd
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--signal", required=True, help="Choose whether compressed (c) or uncompressed (u)")
+args = vars(parser.parse_args())
+signal_mass = args["signal"]
+if not (signal_mass == 'c' or signal_mass == 'compressed' or signal_mass == 'u' or signal_mass == 'uncompressed'):
+    print 'Please enter either \'c\' for the compressed signal region or \'u\' for the uncompressed signal region '
+    sys.exit()
+
+compressed = True if (signal_mass == 'c' or signal_mass == 'compressed') else False
+uncompressed = True if (signal_mass == 'u' or signal_mass == 'uncompressed') else False
 
 ### THRESHOLDS ###
 signal_compressed_inefficiency_threshold = 0.2
@@ -29,14 +39,16 @@ for file_path in best_metrics_paths:
     else:
         metrics = pd.concat([metrics, add_metrics])
 
-metrics = metrics[metrics['sig_comp_ineff'] < signal_compressed_inefficiency_threshold]
-metrics = metrics[metrics['sig_uncomp_ineff'] < signal_uncompressed_inefficiency_threshold]
+if compressed:
+    metrics = metrics[metrics['sig_comp_ineff'] < signal_compressed_inefficiency_threshold]
+    metrics = metrics.sort_values('sig_comp_ineff')
+if uncompressed:
+    metrics = metrics[metrics['sig_uncomp_ineff'] < signal_uncompressed_inefficiency_threshold]
+    metrics = metrics.sort_values('sig_uncomp_ineff')
 metrics = metrics[metrics['bkg_ineff'] < bkg_inefficiency_thershold]
 metrics = metrics[metrics['js1'] < js1_threshold]
 metrics = metrics[metrics['js2'] < js2_threshold]
 metrics = metrics[metrics['1 - roc auc'] < roc_threshold]
-
-metrics = metrics.sort_values('bkg_ineff')
 
 # Print the best models
 num_models = 3
@@ -47,8 +59,10 @@ print "PRINTING TOP " + str(num_models) + " MODELS\n"
 for i in range(num_models):
     print str(i+1) + ". MODEL (" + str(metrics['file'].values[i]) + ")"
     print "Epoch: " + str(metrics['epoch'].values[i])
-    print "Signal inefficiency (compressed): " + str(round(metrics['sig_comp_ineff'].values[i], 4))
-    print "Signal inefficiency (uncompressed): " + str(round(metrics['sig_uncomp_ineff'].values[i], 4))
+    if compressed:
+        print "Signal inefficiency (compressed): " + str(round(metrics['sig_comp_ineff'].values[i], 4))
+    if uncompressed:
+        print "Signal inefficiency (uncompressed): " + str(round(metrics['sig_uncomp_ineff'].values[i], 4))
     print "Background inefficiency: " + str(round(metrics['bkg_ineff'].values[i], 4))
     print "DNN output Jensen-Shannon distance (nJet = [6,7,8] vs. nJet = [4,5]): " + str(round(metrics['js1'].values[i], 4))
     print "DNN output Jensen-Shannon distance (nJet >= 9 vs. nJet = [6,7,8]): " + str(round(metrics['js2'].values[i], 4))
